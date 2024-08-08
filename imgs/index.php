aff46e778a012d75736d9e3ffd86bc179eafa382
fhertecomzt <fhertecorl2@gmail.com>
2024-08-08 14:54:38 -0700

    Comenzar de nuevo

--- imgs/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION["usuario"])){//Extrae la etiqueta de tipo usuario
-	header("Location: ../index.html");
-	}
-	include 'logout.php';
-	header("Location: ../index.html");			
-?>
--- php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start();
-	session_unset(); // Elimina todas las variables de sesión
-	session_destroy(); // Destruye la sesión
-	header("Location: ../login.php"); // Redirige al formulario de inicio de sesión
-	exit;
-?>
